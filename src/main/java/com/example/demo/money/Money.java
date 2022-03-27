1dfe6f485ccc6f59d1c53da25b1ab546d72b3e18
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-03-27 21:56:04 +0900

    テストに聞いてみる

--- src/main/java/com/example/demo/money/Money.java
+++ src/main/java/com/example/demo/money/Money.java
@@ -1,20 +1,26 @@
 package com.example.demo.money;
 
-public abstract class Money {
+public class Money {
   protected int amount;
   protected String currency;
-  Money(int amount, String currency){
+  public Money(int amount, String currency){
     this.amount = amount;
     this.currency = currency;
   }
-  public abstract Money times(int multiplier);
+  public  Money times(int multiplier) {
+    return new Money(amount * multiplier, currency);
+  };
   public String currency() {
     return currency;
   };
 
   public boolean equals(Object object) {
     Money money = (Money) object;
-    return amount == money.amount && getClass().equals(money.getClass());
+    return amount == money.amount && currency().equals(money.currency());
+  }
+  
+  public String toString() {
+    return amount + " " + currency;
   }
   
   public static Money dollar(int amount) {
